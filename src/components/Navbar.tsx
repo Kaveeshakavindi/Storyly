75049fb8259e2ac674abb9c4c1755c5a79793067
Kaveesha Fernando <kaveeshaf@seylan.lk>
2025-01-15 15:14:47 +0530

    fixed a to link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const pathname = usePathname();
   return (
     <div className='flex justify-between items-center p-4 shadow-sm top-0 w-full'>
-      <div><a href='/'>#</a></div>
+      <div><Link href='/'>#</Link></div>
       <div className=''>
         <Link href='/' className={`px-4 hover:text-slate-950 ${pathname === '/' ? 'text-slate-950':'text-slate-500'} `}>Home</Link>
         <Link href='/stories' className={`px-4 hover:text-slate-950 ${pathname === '/stories' ? 'text-slate-950' : 'text-slate-500'}`}>Stories</Link>
